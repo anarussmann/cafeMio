985503a0690dc0efa59d78e86d5c3ad1dedc0867
Ana Russmann <anarussmann@gmail.com>
2022-12-02 15:42:18 -0300

    cree carpetas y movi index

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require('express')
 const app = express()
+const hbs = require('hbs');
+
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+
+app.set('view engine', 'hbs');
 
 app.use(express.static('public'));
 
-   // app.get('/', function (req, res) {
-    //res.send('Hello World')
-    //})
+//ruta raíz de INDEX
+app.get('/', function (req, res) {
+    res.render('index', {
+        titulo: "Mi página web"
+    })
+
+})
+
 
 app.listen(3000, () => {
     console.log ('Listening on port 3000')
