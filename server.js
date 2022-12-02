7524527aca5bee938347a10e88de06a17f0222b0
Ana Russmann <anarussmann@gmail.com>
2022-12-02 15:53:47 -0300

    copie el de santi

--- server.js
+++ server.js
@@ -2,21 +2,25 @@ const express = require('express')
 const app = express()
 const hbs = require('hbs');
 
-hbs.registerPartials(__dirname + '/views/partials', function (err) {});
+const port = 3000
 
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs'); // indica que usaremos la plantilla "handlebars"
+hbs.registerPartials(__dirname + '/views/partials'); // indicamos el directorio hacia los parciales
 
-app.use(express.static('public'));
+app.use(express.static('public')); // indica la carpeta public para considerar archivos estáticos
 
-//ruta raíz de INDEX
-app.get('/', function (req, res) {
-    res.render('index', {
+// Cuando voy a la raiz, entonces se ejecutan las siguientes instrucciones, por ejemplo, responde con renderizar el archivo index, que se encuentra en la carpeta views.
+app.get('/', (req, res) => {
+
+    // (indica el nombre del archivo hbs, objeto)
+    res.render('index',{ 
         titulo: "Mi página web"
     })
 
 })
 
 
-app.listen(3000, () => {
-    console.log ('Listening on port 3000')
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
