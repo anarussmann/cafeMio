f635ec4256b965340c7f8e7406505b4dfd3f634a
Ana Russmann <anarussmann@gmail.com>
2022-12-09 19:44:37 -0300

    probe con el modal de santi no funciono

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 const express = require('express')
 const app = express()
 const hbs = require('hbs');
-const productos = require('./data/products.json')
-const path = require('path');
+//const productos = require('./data/products.json')
+//const path = require('path');
 
 const port = 3000
 
 app.set('view engine', 'hbs'); // indica que usaremos la plantilla "handlebars"
 
-app.set('views',[
+/*app.set('views',[
   path.join('./views/front'),
   path.join('./views/back'),
   path.join('./views')
-])
+])*/
 
 hbs.registerPartials(__dirname + '/views/partials'); // indicamos el directorio hacia los parciales
 
@@ -49,6 +49,9 @@ app.get('/products', (req, res) => {
   res.render('products',{ 
       titulo: "productos",
       productos: productos.products
+      //,
+      //  *******Ana**** ver ojo 
+      //productos: productos[0].data
   })
 
 })
@@ -76,6 +79,8 @@ app.get('/item-producto1', (req, res) => {
   })
 
 })
+
+
 app.get('/item-producto2', (req, res) => {
   // (indica el nombre del archivo hbs, objeto)
   res.render('item-producto2',{ 
@@ -93,7 +98,7 @@ app.get('/item-producto3', (req, res) => {
 
 
 // BACK
-app.get('/admin', (req, res) => {
+/*app.get('/admin', (req, res) => {
 	res.render('admin', {
 		titulo: "Panel de control",
 		productos: productos[0].data
@@ -125,7 +130,7 @@ app.use((req, res, next) => {
 	res.status(404).render('404', {
 		titulo: "404 - No encontrado"
 	})
-})
+})*/
 
 
 
